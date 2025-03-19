86a0039256015355117c07c636f117c4eb57bf81
Ahmed Muhammad <78971171+ahmedmohmd@users.noreply.github.com>
2025-03-19 11:13:27 +0200

    Add problem 5 solution with python lang

---
+++ problems/problem-5.md
@@ -0,0 +1,11 @@
+# Problem (5)
+
+Write a program to ask the user to enter his/her:
+
+- Age
+- Driver license
+- Has Recommendation!
+
+Then Print “Hired” if his\her age is grater than 21 and s/he
+has a driver license, otherwise Print “Rejected”
+Or Hire him\her without conditions!
---
+++ solutions/python/problem-5.py
@@ -0,0 +1,50 @@
+class Solution:
+  max_age = 21
+  
+  def main(self):
+    user_age = self.read_number()
+    has_license = self.read_license()
+    has_recommendation = self.read_recommendation()
+
+    if(has_recommendation):
+      print("Hired")
+    elif(user_age >= Solution.max_age and has_license):
+      print("Hired")
+    else:
+      print("Rejected!")
+
+  def read_number(self):
+    user_input = input("Enter your Age: ")
+
+    if not user_input.isdigit():
+      print("Invalid input. Please enter a valid age.")
+      return self.read_number()
+
+    return int(user_input)
+  
+  def read_license(self):
+    has_license_answers = ["yes", "y"]
+    not_has_license_answers = ["no", "n"]
+
+    user_input = input("Do you have a driver license? (yes/no): ")
+
+    if(user_input.lower() in has_license_answers):
+      return True
+    elif (user_input.lower() in not_has_license_answers):
+      return False
+    else:
+      print("Invalid input. Please enter 'yes' or 'no'.")
+  
+  def read_recommendation(self):
+    has_recommendation_answers = ["yes", "y"]
+    not_has_recommendation_answers = ["no", "n"]
+
+    user_input = input("Do you have a recommendation? (yes/no): ")
+
+    if(user_input.lower() in has_recommendation_answers):
+      return True
+    elif (user_input.lower() in not_has_recommendation_answers):
+      return False
+    else:
+      print("Invalid input. Please enter 'yes' or 'no'.")
+      return self.read_recommendation()
